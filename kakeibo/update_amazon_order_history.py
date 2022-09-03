b255e330ff2fd9806cdfbf7129ada418a6482b78
okayu.f <okayu.f@gmail.com>
2022-09-03 19:48:50 +0900

    amazon履歴取得を一部修正

--- kakeibo/update_amazon_order_history.py
+++ kakeibo/update_amazon_order_history.py
@@ -11,12 +11,12 @@ def execute(wb, sheet_name, order_num_column=0, date_column=2, date_fmt='%Y-%m-%
     print(f'latest_order_num={latest_order_num}')
     detail_amazon_order_history.fetch(latest_order_num)
     df = detail_amazon_order_history.history_to_df()
-    latest_copy.update_data(ws, date_column, latest_row, df, date_fmt, index=True)
+    latest_copy.update_data(ws, date_column, latest_row, df, date_fmt, index=False)
 
 
 if __name__ == '__main__':
     wb = load_workbook("./data/ama_his_test.xlsx")
     execute(wb, 'Ama履歴')
-    wb.save("./ama_his_test_update.xlsx")
+    wb.save("./data/ama_his_test_update.xlsx")
 
     print('done')
